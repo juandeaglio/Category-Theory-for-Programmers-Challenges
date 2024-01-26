8eeac7ecb2757c8f5ae376ba3d3de7b9bd480a8e
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2024-01-26 14:26:40 -0500

    Also implemented variable length args into memoize_function/slow_function

--- CH.2 Challenges/test_memoize_pure_function.py
+++ CH.2 Challenges/test_memoize_pure_function.py
@@ -1,4 +1,6 @@
 """Tests for composing two functions into each other for a resulting function"""
+import math
+import random
 import time
 from typing import TypeVar, Callable
 import pytest
@@ -10,22 +12,22 @@ memoized = {}
 T = TypeVar('T')
 
 
-def memoize_slow_function(func, arg):
-    if memoized.get(arg) is None:
+def memoize_slow_function(func, *argv):
+    if memoized.get(argv) is None:
         usleep(500)
 
-    result = memoize_function(arg, func)
+    result = memoize_function(func, *argv)
     return result
 
 
-def memoize_function(arg, func):
-    if memoized.get(arg) is None:
+def memoize_function(func, *argv):
+    if memoized.get(argv) is None:
         function: Callable[[...], T] = func
-        value: T = function(arg)
-        memoized[arg] = value
+        value: T = function(*argv)
+        memoized[argv] = value
         return value
     else:
-        return memoized.get(arg)
+        return memoized.get(argv)
 
 
 # Implement the composition function in your favorite language. It
@@ -44,3 +46,23 @@ def test_memoize_slow_function():
     memoized_call = end - start
 
     assert memoized_call < not_memoized_call and retval == retval2 and retval == "HELLO!"
+
+
+def test_memoize_slow_pow():
+    start = time.time()
+    retval: str = memoize_slow_function(math.pow, 2, 2)
+    end = time.time()
+    not_memoized_call = end - start
+
+    # now the memoized version
+    start = time.time()
+    retval2: str = memoize_slow_function(math.pow, 2, 2)
+    end = time.time()
+    memoized_call = end - start
+
+    assert memoized_call < not_memoized_call and retval == retval2 and retval == 4
+
+#def test_memoize_random_no_args():
+#    not_memoized = memoize_function(random.random)
+ #   memoized = memoize_function(random.random)
+#    assert memoized != not_memoized
