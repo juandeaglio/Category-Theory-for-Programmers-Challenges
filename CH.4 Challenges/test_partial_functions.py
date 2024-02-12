65b341e06b35e8ec3953346df0173434b3ae5a96
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2024-02-12 11:59:25 -0500

    Added composed reciprocal sqrt and refactored identity.

--- CH.4 Challenges/test_partial_functions.py
+++ CH.4 Challenges/test_partial_functions.py
@@ -1,11 +1,16 @@
 import math
+import pytest
 import unittest
-from typing import TypeVar, Callable, Type
+from fractions import Fraction
+from typing import TypeVar, Callable, Type, Union, Optional
+
+import pytest
 
 T = TypeVar('T')
 
 
-class Optional:
+# Question 1
+class Partial:
     def __init__(self, t: Type[T], _value=None):
         if _value is not None:
             assert isinstance(_value, t)
@@ -22,8 +27,19 @@ class Optional:
         return self._is_valid
 
 
-def identity(x: Type[T]) -> Optional(TypeVar):
-    return Optional(x)
+def safe_reciprocal(x: Fraction) -> Partial(Fraction):
+    if x:
+        assert isinstance(x, Fraction)
+        if x.numerator != 0:
+            # take reciprocal
+            reciprocal = Fraction(numerator=x.denominator, denominator=x.numerator)
+            return Partial(Fraction, reciprocal)
+
+    return Partial(Fraction)
+
+
+def identity(x: Partial) -> Partial:
+    return x
 
 
 def compose_functions(f: Callable, g: Callable) -> Callable:
@@ -39,22 +55,26 @@ def compose_functions(f: Callable, g: Callable) -> Callable:
     return composed_functions
 
 
-def safe_root(x: int) -> Optional(float):
+def safe_root(x: Union[int, float, Fraction]) -> Partial(float):
+    if x:
+        value = x
+        assert isinstance(x, (int, float, Fraction))
+        # Fraction support
+        if isinstance(x, Fraction):
+            value = x.real
+        if x >= 0:
+            return Partial(float, math.sqrt(value))
+
+    return Partial(float)
+
+
+def is_natural_number(x: int) -> Partial(int):
     if x:
         assert isinstance(x, int)
         if x >= 0:
-            return Optional(float, math.sqrt(x))
+            return Partial(int, x)
 
-    return Optional(float)
-
-
-def is_natural_number(x: int) -> Optional(int):
-    if x:
-        assert isinstance(x, int)
-        if x >= 0:
-            return Optional(int, x)
-
-    return Optional(int)
+    return Partial(int)
 
 
 class MyTestCase(unittest.TestCase):
@@ -68,7 +88,8 @@ class MyTestCase(unittest.TestCase):
         assert is_natural_number(1).value() == 1
 
     def test_identity_partial_empty_function(self):
-        assert identity(int).is_valid() is False
+        assert identity(Partial(int)).is_valid() is False
+        assert identity(Partial(int, 1)).is_valid() is True
 
     def test_compose_partial_functions(self):
         composed_function = compose_functions(is_natural_number, safe_root)
@@ -77,6 +98,20 @@ class MyTestCase(unittest.TestCase):
         assert not composed_function(-1).is_valid()
         assert 2 == composed_function(4).value()
 
+    # Question 2
+    def test_safe_reciprocal(self):
+        assert 1 == safe_reciprocal(Fraction(1.0)).value()
+        assert not safe_reciprocal(Fraction(0.0)).is_valid()
+
+    # Question 3
+    def test_compose_reciprocal_sqrt(self):
+        composed: Callable = compose_functions(safe_reciprocal, safe_root)
+
+        result: Partial = composed(Fraction(math.pi))
+        assert result.is_valid()
+
+        expected: float = math.sqrt(1/math.pi)
+        assert pytest.approx(expected) == pytest.approx(result.value())
 
 if __name__ == '__main__':
     unittest.main()
