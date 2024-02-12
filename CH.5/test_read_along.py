a5d567500c3efb343ea62fbbcfa7d2eae40d513b
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2024-02-12 14:34:25 -0500

    Determine common factor from 'm' by reversing p & q after m.

--- CH.5/test_read_along.py
+++ CH.5/test_read_along.py
@@ -1,15 +1,15 @@
 import pytest
-from typing import Tuple, TypeVar, Union
+from typing import Tuple, TypeVar
 
 T = TypeVar('T')
 U = TypeVar('U')
 
 
-def first(pair: Tuple[T, U]) -> Union[T, U]:
+def first(pair: Tuple[T, U]) -> T:
     return pair[0]
 
 
-def second(pair: Tuple[T, U]) -> Union[T, U]:
+def second(pair: Tuple[T, U]) -> U:
     return pair[1]
 
 
@@ -24,6 +24,46 @@ def test_products_functions_first(product_fixture):
 
 def test_products_functions_second(product_fixture):
     assert product_fixture[1] == second(product_fixture)
+
+
+@pytest.fixture()
+def int_bool_fixture() -> Tuple[int, bool]:
+    return 2, True
+
+
+def first_int_bool(pair: Tuple[T, U]) -> T:
+    return pair[0]
+
+
+def second_int_bool(pair: Tuple[T, U]) -> U:
+    return pair[1]
+
+
+mult_common_factor: int = 2
+def mult_factor(x: int) -> Tuple[int, bool]:
+    return x * mult_common_factor, True
+
+
+def test_mult_product_first(int_bool_fixture):
+    assert first(mult_factor(int_bool_fixture[0])) == int_bool_fixture[0] * 2
+
+
+def test_mult_product_second(int_bool_fixture):
+    assert second(mult_factor(int_bool_fixture[0])) == int_bool_fixture[1]
+
+
+# @pytest.fixture()
+# def p_q_fixture():
+#     return 2, True
+
+
+def test_reconstruct_first_from_p_q(int_bool_fixture):
+    common_factor = mult_factor(int_bool_fixture[0])[0] / int_bool_fixture[0]  # this hurts
+    assert common_factor == mult_common_factor
+
+
+def test_reconstruct_second_from_p_q(int_bool_fixture):
+    assert int_bool_fixture[1] == mult_factor(int_bool_fixture[1])[1]  # somehow this works, true is treated as 1 underlying impl?
 
 
 if __name__ == '__main__':
