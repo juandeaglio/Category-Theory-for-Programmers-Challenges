b42196e644cf831534cd2ab8646713cf3aaa23de
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2024-02-14 14:54:03 -0500

    CH 5 Challenges Question 1 with illustrations!

---
+++ CH.5/test_chapter_five.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class ChapterFive(unittest.TestCase):
+    # Question 1
+    def test_unique_up_to_unique_isomorphism(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
