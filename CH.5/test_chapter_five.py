a92fd5e19741eb0672473ebff36a8f507933b8e2
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2024-02-15 16:36:02 -0500

    Coproducts "better" or not better than injections

--- CH.5/test_chapter_five.py
+++ CH.5/test_chapter_five.py
@@ -14,13 +14,13 @@ def divide(x: int, y: int) -> Union[str, int]:
 
 class ChapterFive(unittest.TestCase):
     # Question 1
-    def test_either_type(self):
+    def test_product_type(self):
         x = 6
         y = 2
         result: int = divide(x, y)
         self.assertTrue(isinstance(result, Union[str, int]))
 
-    def test_either_second_type(self):
+    def test_product_second_type(self):
         x = 6
         y = 0
         result: int = divide(x, y)
@@ -31,5 +31,74 @@ class ChapterFive(unittest.TestCase):
         y = 1
         result: int = divide(x, y)
         self.assertFalse(isinstance(result, Union[str, int]))
+
+    def test_co_product_better_than_injections(self):
+        # Injections
+        def int_injection(n: int) -> int:
+            return n
+
+        def bool_injection(b: bool) -> int:
+            return int(b)
+
+        # Maps the following into this an int:
+        #   b       n
+        #   |       |
+        #   |m      |m
+        #   Union   Union
+        #   |       |
+        #   |       |
+        #   v       v
+        #  bool---->int
+        #       c_i (i.e., cast to int)
+
+        # Factorizing function
+        def unionizer(e: Union[int, bool]) -> int:
+            return int(e)
+
+        value = 5
+        self.assertEqual(int_injection(value), unionizer(value))
+        value = True
+        self.assertEqual(bool_injection(value), unionizer(value))
+
+    def test_co_product_better_than_indirect_injections(self):
+        # Injections
+        def int_injection(n: int) -> int:
+            if n < 0:
+                return n
+            else:
+                return n + 2
+
+        def bool_injection(b: bool) -> int:
+            return int(b)
+
+        # Maps the following into this an int:
+        #   b       n
+        #   |       |
+        #   |m      |m
+        #   Union   Union
+        #   |       |
+        #   |       |
+        #   v       v
+        #  bool---->int
+        #       c_i (i.e., cast to int)
+
+        # Factorizing function
+        def unionizer(e: Union[int, bool]) -> int:
+            if isinstance(e, bool):
+                return 0 if e is False else 1
+            elif isinstance(e, int):
+                if e < 0:
+                    return e
+                else:
+                    return e + 2
+
+        value = 1
+        self.assertEqual(int_injection(value), unionizer(value))
+        value = -1
+        self.assertEqual(int_injection(value), unionizer(value))
+        value = True
+        self.assertEqual(bool_injection(value), unionizer(value))
+
+
 if __name__ == '__main__':
     unittest.main()
