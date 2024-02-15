6eb2b575cfe4269d37d42cd4ad8ca75432707b38
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2024-02-15 14:44:38 -0500

    Either type is implemented in Python as a Union.

--- CH.5/test_chapter_five.py
+++ CH.5/test_chapter_five.py
@@ -1,11 +1,35 @@
 import unittest
+from typing import Union
 
+
+def divide(x: int, y: int) -> Union[str, int]:
+    if y == 0:
+        return "Division by zero error."
+
+    elif y == 1:
+        return x / y
+
+    else:
+        return x // y
 
 class ChapterFive(unittest.TestCase):
     # Question 1
-    def test_unique_up_to_unique_isomorphism(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_either_type(self):
+        x = 6
+        y = 2
+        result: int = divide(x, y)
+        self.assertTrue(isinstance(result, Union[str, int]))
 
+    def test_either_second_type(self):
+        x = 6
+        y = 0
+        result: int = divide(x, y)
+        self.assertTrue(isinstance(result, Union[str, int]))
 
+    def test_either_invalid(self):
+        x = 6
+        y = 1
+        result: int = divide(x, y)
+        self.assertFalse(isinstance(result, Union[str, int]))
 if __name__ == '__main__':
     unittest.main()
